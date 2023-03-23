e3462999d9e992a540e17f8e2f7817ad233efeb5
yashelter <yashelter@yandex.ru>
2023-03-23 22:06:31 +0300

    update to unity 2021.3.21f1, починено и дороботаны трубы, всё работает

--- Assets/Scripts/PuzzleScripts/PuzzleManager.cs
+++ Assets/Scripts/PuzzleScripts/PuzzleManager.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,67 +6,34 @@ using UnityEngine.UI;
 
 public class PuzzleManager : MonoBehaviour
 {
-    [SerializeField] private GameObject[] puzzle;
-    [SerializeField] private GameObject PipesHolder;
+    [SerializeField] private PuzzleRotate[] puzzles;
+    public int allTiles = 6;
+    public int solvedTiles = 6;
     
-    [SerializeField] private int totalPipes = 0;
-
-    int correctPipes = 0;
-
-
-
     private void Start()
     {
-        totalPipes = PipesHolder.transform.childCount;
-
-        puzzle = new GameObject[totalPipes];
-
-        for (int i = 0; i < puzzle.Length; i++)
-        {
-            puzzle[i] = PipesHolder.transform.GetChild(i).gameObject;
-        }
-
-
+        puzzles = FindObjectsOfType<PuzzleRotate>();
     }
-    
-    public void CorrectMove()
+    public void GenerateField()
     {
-        correctPipes++;
-
-        if (correctPipes == totalPipes)
-        {
-            Debug.Log("YOU WON");
-        }
+        // paste your code here
     }
-    public void WrongMove()
+    public void CheckPuzzle()
     {
-        correctPipes--;
-    }
-}
-/*
-        if (PuzzleRotate.isMouse)
+        solvedTiles = 0;
+        for (int i = 0; i < puzzles.Length; i++)
         {
-            bool AllTrue = true;
-            foreach (var item in puzzle)
+            if (puzzles[i].isPlaced)
             {
-                if (item.transform.rotation.z < -0.01 || item.transform.rotation.z > 0.01)
-                {
-                    AllTrue = false;
-                    break;
-                }
+                solvedTiles++;
             }
-            if (AllTrue)
-            {
-                isCorrect = true;
-                Debug.Log("YOU WON");
-                
-            }
-            PuzzleRotate.isMouse = false;
-        } 
-
-
-        
+        }
+       
+        if (solvedTiles == allTiles)
+        {
+            // send action to other scripts
+            Debug.Log("solved");
+        }
         
     }
 }
-*/
--- Assets/Scripts/PuzzleScripts/PuzzleRotate.cs
+++ Assets/Scripts/PuzzleScripts/PuzzleRotate.cs
@@ -4,16 +4,12 @@ using UnityEngine;
 
 public class PuzzleRotate : MonoBehaviour
 {
-    [SerializeField] private bool isPlaced;
+    public float currentDegrees = 0f;
+    public bool isPlaced;
 
-    
-    
     public float[] correctRotation;
-
-    
-    int PossibleRots = 1;
-
-    PuzzleManager puzzleManager;
+    private float[] rotate = { 0, 90, 180, 270 };
+    private PuzzleManager puzzleManager;
 
     private void Awake()
     {
@@ -22,61 +18,29 @@ public class PuzzleRotate : MonoBehaviour
 
     private void Start()
     {
-         float[] rotate = { 0, 90, 180, 270 };
+        int Rand = Random.Range(0, rotate.Length); // надо учесть что пазл может стать решённым сразу
 
-    PossibleRots = correctRotation.Length;
-        int Rand = Random.Range(0, rotate.Length);
-        transform.Rotate(0, 0, rotate[Random.Range(0, rotate.Length)]);
         transform.eulerAngles = new Vector3(0,0, rotate[Rand]);
-
-
-        if (PossibleRots == 2)
-        {
-            if (transform.eulerAngles.z == correctRotation[0] || transform.eulerAngles.z == correctRotation[1] || transform.eulerAngles.z == -correctRotation[1])
-            {
-                isPlaced = true;
-                puzzleManager.CorrectMove();
-            }
-        } else if (PossibleRots == 1) 
-        {
-            if (transform.eulerAngles.z == correctRotation[0])
-            {
-                isPlaced = true;
-                puzzleManager.CorrectMove();
-            }
-        }
-
-
+        currentDegrees = rotate[Rand];
+        isPlaced = CheckTile();
+        
     }
     private void OnMouseDown()
     {
         transform.Rotate(0, 0, 90);
-
-        if (PossibleRots > 1)
+        currentDegrees = ((currentDegrees + 90) % 360);
+        isPlaced = CheckTile();
+        puzzleManager.CheckPuzzle();
+    }
+    private bool CheckTile()
+    {
+        for (int i = 0; i < correctRotation.Length; i++)
         {
-            if ((transform.eulerAngles.z == correctRotation[0] || transform.eulerAngles.z == correctRotation[1] || transform.eulerAngles.z == -correctRotation[1]) && isPlaced == false)
+            if (currentDegrees == correctRotation[i])
             {
-                isPlaced = true;
-                puzzleManager.CorrectMove();
-            }
-            else if (isPlaced == true)
-            {
-                isPlaced = false;
-                puzzleManager.WrongMove();
-            }
-        } else if (PossibleRots == 1)
-        {
-            if (transform.eulerAngles.z == correctRotation[0] && isPlaced == false)
-            {
-                isPlaced = true;
-                puzzleManager.CorrectMove();
-            }
-            else if (isPlaced == true)
-            {
-                isPlaced = false;
-                puzzleManager.WrongMove();
-            }
+                return true;
+            }   
         }
-        
+        return false;
     }
 }
