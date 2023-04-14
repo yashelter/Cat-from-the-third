7d44de8fff1bb2cb202b78f537f8c9543615bd01
rob228rob@gmail.com <rob228rob@gmail.com>
2023-04-15 01:14:25 +0300

    Improved version "MathGame"

--- Assets/Scripts/MathGame/MathProblem.cs
+++ Assets/Scripts/MathGame/MathProblem.cs
@@ -8,14 +8,15 @@ public class MathProblem : MonoBehaviour
 {
     public TMP_Text firstNum;
     public TMP_Text secondNum;
-    public TMP_Text firstAnswer;
-    public TMP_Text secondAnswer;
-    public TMP_Text thirdAnswer;
-    public TMP_Text operatorProbl;
-    //public List<int> easyMathList = new List<int>();
 
-    public int randFirst;
-    public int randSecond;
+    public TMP_Text[] answers;
+    
+    public TMP_Text operatorProbl;
+    public TMP_Text xAnsw;
+
+    public Button[] buttons = new Button[3];
+    public Color colorWin;
+    public Color colorLose;
 
     private int firstNumProbl;
     private int secondNumProbl;
@@ -35,18 +36,24 @@ public class MathProblem : MonoBehaviour
         DisplayMathProblem();
     }
 
-    private int GenerateUniq(int firstNum, int secondNum)
+    private int GenerateUniq(int currNum, int prevNum)
     {
         //Generate uniq number
-        return 0;
+        do
+        {
+            currNum = Random.Range(prevNum - 50, prevNum + 50);
+        } while (currNum == prevNum);
+        
+        
+        return currNum;
     }
 
     private void SetProblemNums(int firstNum, int secondNum, int oper)
     {
         if (oper == 0) // equal +
         {
-            randFirst = Random.Range(-50, 50);
-            randSecond = Random.Range(0, 100);
+            int randFirst = Random.Range(-100, 100);
+            int randSecond = Random.Range(0, 150);
 
             firstNumProbl = randFirst;
             secondNumProbl = randSecond;
@@ -54,8 +61,8 @@ public class MathProblem : MonoBehaviour
         } 
         else if (oper == 1) // equal -
         {
-            randFirst = Random.Range(1, 150);
-            randSecond = Random.Range(0, 100);
+            int randFirst = Random.Range(50, 200);
+            int randSecond = Random.Range(0, 150);
 
             firstNumProbl = randFirst;
             secondNumProbl = randSecond;
@@ -63,8 +70,8 @@ public class MathProblem : MonoBehaviour
         } 
         else if (oper == 2) // equal *
         {
-            randFirst = Random.Range(3, 10);
-            randSecond = Random.Range(4, 15);
+            int randFirst = Random.Range(3, 10);
+            int randSecond = Random.Range(4, 15);
 
             firstNumProbl = randFirst;
             secondNumProbl = randSecond;
@@ -73,11 +80,8 @@ public class MathProblem : MonoBehaviour
         //Generate first and second number for our prolem
     }
 
-    //generate random nums
     public void DisplayMathProblem()
     {
-        //Random rnd = new Random();
-
 
         int rnd = Random.Range(0, 3);
         if (rnd == 0) // eq +
@@ -97,11 +101,11 @@ public class MathProblem : MonoBehaviour
             SetProblemNums(firstNumProbl, secondNumProbl, 2);
             answCorrect = firstNumProbl * secondNumProbl;
             operatorProbl.text = "*";
-        } 
-       
-        //answCorrect = firstNumProbl + secondNumProbl; // create a generate func to the operator {+,-,*,/}
-        answTwo = Random.Range(firstNumProbl - 40, secondNumProbl + 40); // 
-        answThree = Random.Range(firstNumProbl - 45, secondNumProbl + 45);
+        }
+
+        
+        answTwo = GenerateUniq(answTwo, answCorrect); // Genreate uniq wrong answers
+        answThree = GenerateUniq(answThree, answTwo);
 
         firstNum.text = "" + firstNumProbl;
         secondNum.text = "" + secondNumProbl;
@@ -109,22 +113,41 @@ public class MathProblem : MonoBehaviour
         randPlacement = Random.Range(0, 3);
         if (randPlacement == 0)
         {
-            firstAnswer.text = "" + answCorrect;
-            secondAnswer.text = "" + answTwo;
-            thirdAnswer.text = "" + answThree;
+            answers[0].text = "" + answCorrect;
+            answers[1].text = "" + answTwo;
+            answers[2].text = "" + answThree;
             currentAnsw = 0;
         } else if (randPlacement == 1) 
         {
-            secondAnswer.text = "" + answCorrect;
-            firstAnswer.text = "" + answTwo;
-            thirdAnswer.text = "" + answThree;
+            answers[1].text = "" + answCorrect;
+            answers[0].text = "" + answTwo;
+            answers[2].text = "" + answThree;
             currentAnsw = 1;
         } else if (randPlacement == 2)
         {
-            firstAnswer.text = "" + answTwo;
-            secondAnswer.text = "" + answThree;
-            thirdAnswer.text = "" + answCorrect;
+            answers[0].text = "" + answTwo;
+            answers[1].text = "" + answThree;
+            answers[2].text = "" + answCorrect;
             currentAnsw = 2;
+        }
+    }
+
+    private void SetColor(Button button, int status)
+    {
+        if (status == 1) // 1 equal win
+        {
+            ColorBlock cb = button.colors;
+            cb.pressedColor = colorWin;
+            cb.highlightedColor = colorWin;
+            //cb.normalColor = colorWin;  //   - create some bugs
+            button.colors = cb;
+        } 
+        else if (status == 0) // 0 equal lose
+        {
+            ColorBlock cb = button.colors;
+            cb.pressedColor = colorLose;
+            cb.highlightedColor = colorLose;
+            button.colors = cb;
         }
     }
 
@@ -132,10 +155,14 @@ public class MathProblem : MonoBehaviour
     {
         if (currentAnsw == 0)
         {
+            //SetColor(buttons[0], 1); //may be some bugs
             Debug.Log("You win");
+            xAnsw.text = "" + answCorrect;
+
         } else
         {
-            Debug.Log("Try Again");
+            //SetColor(buttons[0], 0); //may be some bugs
+            xAnsw.text = "x";
             DisplayMathProblem();
         }
     }
@@ -144,11 +171,14 @@ public class MathProblem : MonoBehaviour
     {
         if (currentAnsw == 1)
         {
+           // SetColor(buttons[1], 1); //may be some bugs
             Debug.Log("You win");
+            xAnsw.text = "" + answCorrect;
         }
         else
         {
-            Debug.Log("Try Again");
+           // SetColor(buttons[1], 0); //may be some bugs
+            xAnsw.text = "x";
             DisplayMathProblem();
         }
     }
@@ -158,11 +188,14 @@ public class MathProblem : MonoBehaviour
     {
         if (currentAnsw == 2)
         {
+            SetColor(buttons[2], 1); //may be some bugs
             Debug.Log("You win");
+            xAnsw.text = "" + answCorrect;
         }
         else
         {
-            Debug.Log("Try Again");
+            SetColor(buttons[2], 0); //may be some bugs
+            xAnsw.text = "x";
             DisplayMathProblem();
         }
     }
